1bdc20b713247b89f1b5bcbff37d9d7d1f3b4a09
Justin Goyena <jgoyen@Justins-MacBook-Pro.local>
2019-07-02 20:20:43 -0700

    updated random string to pull up a random letter. Was able to get counter to pop up on html page

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,33 +1,35 @@
-// function randomString() {
-// 	var chars = "abcdefghiklmnopqrstuvwxyz";
-// 	var string_length = 1;
-// 	var randomstring = '';
-// 	for (var i=0; i<string_length; i++) {
-// 		var rnum = Math.floor(Math.random() * chars.length);
-// 		randomstring += chars.substring(rnum,rnum+1);
-// 	}
-// 	document.randform.randomfield.value = randomstring;
-// }
+function randomString() {
+    var chars =["a","b","c","d","e","f","g","h","i","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+	var string_length = 1;
+	var randomstring = '';
+	for (var i=0; i<string_length; i++) {
+		var rnum = chars[Math.floor(Math.random() * chars.length)];
+        randomstring += chars.substring(rnum,rnum+1);
+        console.log(chars)
+	}
+	document.onekeydown = randomString;
+}
 
 
 var counter = 0;
 
 function writeCounter(){
+    console.log("executingwritecounterfunction")
     //this event object available to us
     var userGuesses = ['a', 'b', 'c'];
 
     if (userGuesses.includes(event.key)){
         counter++;
-
+        document.getElementById("counter").innerHTML = 'Your guesses so far: ' + counter;
         //when you use document.write it overrides the entire html page and gets rid of all the javascript and css
         // document.write(counter);
-        console.log();
+        console.log(counter);
     }
 
 }
-document.onekeydown = writeCounter;
+document.onkeyup = writeCounter;
 
-document.getElementById("counter").innerHTML = counter;
+
 
 // var userLetter;
 // var randomLetter = ["a","b","c"];
