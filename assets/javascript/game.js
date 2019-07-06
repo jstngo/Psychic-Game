c5fcf1c3df514ca94c0ebbe678e8e124b340ae00
Justin Goyena <jgoyen@Justins-MBP.attlocal.net>
2019-07-05 20:38:15 -0700

    added span ids for wins, losses guesses left, and your guesses so far. Added score tracker for each using if statements and alerts when user wins or loses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,4 @@
-var storage = 10;
-
 var letters = [];
-
-var pTag = document.querySelector('p');
 
 var newletters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
     
@@ -10,128 +6,61 @@ var newletter = newletters[Math.floor(Math.random()*newletters.length)];
 
 console.log(newletter);
 
-pTag.innerText = storage;
+var guessesLeft = 10;
 
+var elGuessesLeft = document.querySelector("#guessesLeft");
 
+elGuessesLeft.innerText = guessesLeft;
+
+var userVal = document.querySelector("#currentGuesses");
+
+userVal.innerText = letters;
+
+var lettersMatch = 0;
+
+var matchingLetters = document.querySelector("#winTracker");
+
+matchingLetters.innerText = lettersMatch;
+
+var lossScore = 0;
+
+var lostOne = document.querySelector("#lossTracker");
+
+lostOne.innerText = lossScore;
 
 function increase(){
-    storage--;
-
-    pTag.innerText = storage;
-    letters.push(event.key);
-    document.querySelector("#counter").innerText = 'Your guesses so far: ' + letters;
-    return letters;
-
-    }
     
+    guessesLeft--;
+    elGuessesLeft.innerText = guessesLeft;
+    // console.log(guessesLeft);
+    letters.push(event.key);
+    userVal.innerText = letters;
+    // document.querySelector("#counter").innerText = 'Your guesses so far: ' + letters;
+    return letters;
+}
 
 document.onkeyup = increase;
 
 function match(event) {
     var x = event.key;
-  
-    // If the pressed keyboard button is "a" or "A" (using caps lock or shift), alert some text.
+
     if (x == newletter) { 
+      lettersMatch++;
+      matchingLetters.innerText = lettersMatch;
       alert ("You did it!");
     }
   }
 
-
-
 document.onkeydown = match;
 
-// function randomLetter( ) {
+function loss(){
+    
+      if (guessesLeft == 0) {
+        console.log(guessesLeft);
+        lossScore++;
+        lostOne.innerText = lossScore;
+        alert ("You lost this round");
+      }
+}
 
-
-//     console.log(newletter);
-
-//     }
-
-//     randomLetter();
-
-
-//     document.onkeydown = randomLetter;
-
-
-// function randomString() {
-//     var chars =["a","b","c","d","e","f","g","h","i","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-//     //chars letters string
-//     var string_length = 1;
-// 	var randomString = '';
-// 	for (var i=0; i<string_length; i++) {
-// 		var rnum = chars[Math.floor(Math.random() * chars.length)];
-//         randomString += chars.substring(rnum,rnum+1);
-//         alert(rnum);
-//         console.log(randomString);
-// 	}
-//     document.onkeydown = randomString;
-
-// }
-
-// randomString();
-
-// var counter = 0;
-
-// function writeCounter(){
-//     console.log("executingwritecounterfunction")
-//     //this event object available to us
-//     var userGuesses = ['a', 'b', 'c'];
-
-//     if (userGuesses.includes(randomString)){
-//         counter++;
-//         document.getElementById("counter").innerHTML = 'Your guesses so far: ' + counter;
-//         //when you use document.write it overrides the entire html page and gets rid of all the javascript and css
-//         // document.write(counter);
-//         console.log(counter);
-//     }
-
-// }
-// document.onkeyup = writeCounter;
-
-
-
-// var userLetter;
-// var randomLetter = ["a","b","c"];
-
-// var losses = 0;
-// var guessesRemaining = 10;
-// var guessedSoFar = 0;
-
-
-
-
-// var wins = 0;
-
-// function writeWins(){
-//     //this event object available to us
-//     var randomLetter = ['a', 'b', 'c'];
-//     var pTags = document.querySelectorAll('p');
-//     if (randomLetter.includes(event.key)){
-//         wins++;
-//         pTags[2].innerText = '';
-
-//         //when you use document.write it overrides the entire html page and gets rid of all the javascript and css
-//         // document.write(counter);
-//     }
-// }
-
-// function userLetter(a){
-//     console.log(a)
-// }
-
-// function randomLetter(b){
-//     console.log(b)
-// }
-
-// if (userLetter.includes(event.key)){
-//     wins++;
-//     alert(wins)
-// }
-
-// else {
-//     console.log(event.key + " wrong");
-// }
-
-// function wins(a,b){
-//     console.log(a==b)
-// }
+document.onkeydown = loss;
